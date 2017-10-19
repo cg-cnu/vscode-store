316df195a9cae2b511f0ce26926116ee885667ca
cg-cnu <sreenivas9alapati@gmail.com>
2017-10-19 13:34:31 +0530

    fix: read store path from config

--- src/store.ts
+++ src/store.ts
@@ -4,32 +4,33 @@ import * as clipboard from 'clipboardy';
 import * as fs from 'fs';
 import * as path from 'path';
 
+// get the store file path
 function getStoreFile() {
-    // TODO: created by admin @ 2017-10-19 11:07:33
-    // need to add this to the config
     // get from config 
-    var config: string = vscode.workspace.getConfiguration().get('store.rootPath');
-    // 
-    if (!config) {
-        config = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+    var storePath: string = vscode.workspace.getConfiguration().get('store.path');
+    if (!storePath) {
+        storePath = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
     }
-    const storeFilePath = path.join(config, '.vscode-store.json');
+    const storeFilePath = path.join(storePath, '.vscode-store.json');
     if (!fs.existsSync(storeFilePath)) {
         fs.writeFileSync(storeFilePath, '{}');
     };
     return storeFilePath;
 }
 
+// read store file
 function readStore() {
     return JSON.parse(fs.readFileSync(getStoreFile(), "utf8"));
 }
 
+// write data to store file
 function writeStore(data) {
     fs.writeFileSync(getStoreFile(), JSON.stringify(data), 'utf8');
 }
 
-
 export const activate = (context: vscode.ExtensionContext) => {
+
+    // Open store file
     const openStore = vscode.commands.registerCommand('store.open', () => {
         const storePath = getStoreFile();
         vscode.workspace.openTextDocument(vscode.Uri.file(storePath))
@@ -39,10 +40,12 @@ export const activate = (context: vscode.ExtensionContext) => {
 
     });
 
+    // Create new store
     const newStore = vscode.commands.registerCommand('store.new', () => {
         vscode.window.showInputBox({
-            placeHolder: "Enter new key to store",
-            ignoreFocusOut: true
+            placeHolder: "Enter new key",
+            ignoreFocusOut: true,
+            prompt: "Enter new key"
         }).then((key) => {
             if (!key) {
                 return
@@ -50,16 +53,16 @@ export const activate = (context: vscode.ExtensionContext) => {
             const storeData = readStore();
             // warn the user if the key exists.
             if (new Set(Object.keys(storeData)).has(key.trim())) {
-                vscode.window.showWarningMessage('Key already exists. Try removing it and add again.')
+                vscode.window.showWarningMessage('Key already exists.')
                 return;
             }
             const editor = vscode.window.activeTextEditor;
             vscode.window.showInputBox({
                 ignoreFocusOut: true,
-                placeHolder: "Enter value to store",
+                placeHolder: "Enter value",
                 // prefill the selected text
                 value: editor ? editor.document.getText(editor.selection) : '',
-                prompt: "Enter value to store"
+                prompt: "Enter value"
             }).then(value => {
                 if (!value) {
                     return
@@ -73,6 +76,7 @@ export const activate = (context: vscode.ExtensionContext) => {
 
     context.subscriptions.push(newStore);
 
+    // Get data from store
     const getStore = vscode.commands.registerCommand('store.get', () => {
         // read the json from disk
         const storeData = readStore();
@@ -82,8 +86,6 @@ export const activate = (context: vscode.ExtensionContext) => {
                     return
                 }
                 const value = storeData[key.trim()];   // get the value
-                // IDEA: logged by admin @ 2017-10-16 19:28:53
-                // Show the confirmation based on the config
                 vscode.window.showInformationMessage(value);
                 clipboard.writeSync(value); // Copy the value info to clipboard
 
@@ -92,6 +94,7 @@ export const activate = (context: vscode.ExtensionContext) => {
 
     context.subscriptions.push(getStore);
 
+    // Remove data from store
     const removeStore = vscode.commands.registerCommand('store.remove', () => {
         const storeData = readStore();
         vscode.window.showQuickPick(Object.keys(storeData))
